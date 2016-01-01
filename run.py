c4a355b9a419bd93bf63e7ac1a0dc33618e8e2cb
MacBook air <macbookair@MacBooks-MacBook-Air.local>
2016-01-01 03:24:31 +0300

    Greetings

--- run.py
+++ run.py
@@ -1,13 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__) # Initialized a flask app
 
 
-@app.route('/')
+@app.route('/hello', methods=['GET'])
 def hello():
     return jsonify(
-        {"message": 'Hello youre welcom'}
+        {"message": 'Hello your welcome'}
     )
-
-
+    
+@app.route('/hello', methods=['POST'])
+def greetings():
+    print(request.json)
+    name = request.json['name']
+    print(name)
+    return jsonify(
+            {"message": "Hello "+ name}
+    )
 if __name__ == '__main__':
     app.run(debug=True)## Shows all error messages and bugs
+
+
