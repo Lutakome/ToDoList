642795577a76fed75605ea6dca17889fb6ce4536
MacBook air <macbookair@MacBooks-MacBook-Air.local>
2019-08-06 17:15:18 +0300

    Credentials

--- run.py
+++ run.py
@@ -10,12 +10,44 @@ def hello():
     
 @app.route('/hello', methods=['POST'])
 def greetings():
-    print(request.json)
+    # print(request.json)
     name = request.json['name']
-    print(name)
+    # print(name)
     return jsonify(
             {"message": "Hello "+ name}
     )
+
+@app.route('/hello', methods=['PUT'])
+def edit_name():
+     # print(request.json)
+    name = request.json['name']
+    # print(name)
+    return jsonify(
+            {"message": "Hello "+ name}
+    )
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        if not request.json or 'username' not in request.json or 'password' not in request.json:
+            return jsonify({
+            "error": "username and password are required"
+        }) 
+    except:
+        return jsonify({
+            "error": "Something went wrong"
+        })
+    username = request.json['username']
+    password = request.json['password']
+
+    
+
+    print(request.json['username'])
+
+    return jsonify({
+        "message": "message"
+    })
+    
+
 if __name__ == '__main__':
     app.run(debug=True)## Shows all error messages and bugs
 
